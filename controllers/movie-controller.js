afb16025f707555ad0fd4f20593f0318bcd4021d
Palm-Thanyawit <thanyawit.nitipornwattana@gmail.com>
2023-12-02 13:26:21 +0700

    Clear log2

--- controllers/movie-controller.js
+++ controllers/movie-controller.js
@@ -1,11 +1,6 @@
 
 const BASE_URL = "https://api.themoviedb.org/3/movie/top_rated"
-// GET /watch/list: ดึงข้อมูลหนังทั้งหมด
-// GET /watch/list/{id}: ดึงรายละเอียดของหนังที่ดูตามรหัส
-// GET /search/?q=xxxx: ค้นหารายการหนังจากชื่อ
 const apiKey = process.env.API_KEY
-
-
 exports.showList = async (req,res,next)=>{
     try {
       const response = await fetch(`${BASE_URL}?api_key=${apiKey}`);
