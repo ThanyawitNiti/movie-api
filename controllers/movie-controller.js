e7a78c148ebc960693ad0bf3f627bf54bf86ce28
Palm-Thanyawit <thanyawit.nitipornwattana@gmail.com>
2023-12-02 13:26:09 +0700

    Clear log

--- controllers/movie-controller.js
+++ controllers/movie-controller.js
@@ -1,27 +1,18 @@
-const fs = require('fs/promises')
 
 const BASE_URL = "https://api.themoviedb.org/3/movie/top_rated"
 // GET /watch/list: ดึงข้อมูลหนังทั้งหมด
 // GET /watch/list/{id}: ดึงรายละเอียดของหนังที่ดูตามรหัส
 // GET /search/?q=xxxx: ค้นหารายการหนังจากชื่อ
 const apiKey = process.env.API_KEY
-// Example: Fetch popular movies
-fetch(`${BASE_URL}?api_key=${apiKey}`)
-  .then(response => response.json())
-  .then(data => {
-    // Handle the data here
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
 
 
 exports.showList = async (req,res,next)=>{
     try {
-        
+      const response = await fetch(`${BASE_URL}?api_key=${apiKey}`);
+      const data = await response.json();
+      const {results} = data
+      res.status(200).json(results)
     } catch (error) {
         console.log(error)
-        
     }
 }
--- routes/movie-route.js
+++ routes/movie-route.js
@@ -2,10 +2,6 @@ const express = require("express")
 const movieController = require('../controllers/movie-controller')
 const router = express.Router()
 
-// GET /watch/list: ดึงข้อมูลหนังทั้งหมด
-// GET /watch/list/{id}: ดึงรายละเอียดของหนังที่ดูตามรหัส
-// GET /search/?q=xxxx: ค้นหารายการหนังจากชื่อ
-
 router.get('/watch/list',movieController.showList)
 
 module.exports = router
